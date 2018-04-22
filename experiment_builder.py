Implemented proper full context embeddings, added first steps towards prototypical nets by using mean of per class embeddings and added parsers, also fixed a bug regarding the double softmax that made optimization harder than it should be

experiment_builder.py
@@ -15,7 +15,8 @@ class ExperimentBuilder:
         """
         self.data = data
 
-    def build_experiment(self, batch_size, classes_per_set, samples_per_class, fce, num_gpus=1, data_augmentation=True):
+    def build_experiment(self, batch_size, classes_per_set, samples_per_class, fce, args, full_context_unroll_k=5,
+                         num_gpus=1, data_augmentation=True):
 
         """
 
@@ -27,23 +28,29 @@ class ExperimentBuilder:
         :return: a matching_network object, along with the losses, the training ops and the init op
         """
         height, width, channels = self.data.dataset.image_height, self.data.dataset.image_width, \
-                                  self.data.dataset.image_channel #missing
-        self.support_set_images = tf.placeholder(tf.float32, [num_gpus, batch_size, classes_per_set, samples_per_class, height, width,
-                                                              channels], 'support_set_images')
-        self.support_set_labels = tf.placeholder(tf.int32, [num_gpus, batch_size, classes_per_set, samples_per_class], 'support_set_labels')
+                                  self.data.dataset.image_channel  # missing
+        self.support_set_images = tf.placeholder(tf.float32,
+                                                 [num_gpus, batch_size, classes_per_set, samples_per_class, height,
+                                                  width,
+                                                  channels], 'support_set_images')
+        self.support_set_labels = tf.placeholder(tf.int32, [num_gpus, batch_size, classes_per_set, samples_per_class],
+                                                 'support_set_labels')
         self.target_image = tf.placeholder(tf.float32, [num_gpus, batch_size, height, width, channels], 'target_image')
         self.target_label = tf.placeholder(tf.int32, [num_gpus, batch_size], 'target_label')
         self.training_phase = tf.placeholder(tf.bool, name='training-flag')
         self.dropout_rate = tf.placeholder(tf.float32, name='dropout-prob')
         self.current_learning_rate = 1e-03
         self.learning_rate = tf.placeholder(tf.float32, name='learning-rate-set')
+        self.args = args
         self.one_shot_omniglot = MatchingNetwork(batch_size=batch_size, support_set_images=self.support_set_images,
-                                            support_set_labels=self.support_set_labels,
-                                            target_image=self.target_image, target_label=self.target_label,
-                                            dropout_rate=self.dropout_rate, num_channels=channels,
-                                            is_training=self.training_phase, fce=fce,
-                                            num_classes_per_set=classes_per_set,
-                                            num_samples_per_class=samples_per_class, learning_rate=self.learning_rate)
+                                                 support_set_labels=self.support_set_labels,
+                                                 target_image=self.target_image, target_label=self.target_label,
+                                                 dropout_rate=self.dropout_rate, num_channels=channels,
+                                                 is_training=self.training_phase, fce=fce,
+                                                 num_classes_per_set=classes_per_set,
+                                                 num_samples_per_class=samples_per_class,
+                                                 learning_rate=self.learning_rate,
+                                                 full_context_unroll_k=full_context_unroll_k)
         self.data_augmentation = data_augmentation
         summary, self.losses, self.c_error_opt_op = self.one_shot_omniglot.init_train()
         init = tf.global_variables_initializer()
@@ -61,13 +68,15 @@ class ExperimentBuilder:
         total_train_accuracy = []
         with tqdm.tqdm(total=total_train_batches) as pbar:
             for sample_id, train_sample in enumerate(self.data.get_train_batches(total_batches=total_train_batches,
-                                                                            augment_images=self.data_augmentation)):
+                                                                                 augment_images=self.data_augmentation)):
 
                 support_set_images, target_set_image, support_set_labels, target_set_label = train_sample
 
                 _, c_loss_value, acc = sess.run(
-                    [self.c_error_opt_op, self.losses[self.one_shot_omniglot.classify], self.losses[self.one_shot_omniglot.dn]],
-                    feed_dict={self.dropout_rate: 0.0, self.support_set_images: support_set_images[0],
+                    [self.c_error_opt_op, self.losses[self.one_shot_omniglot.classify],
+                     self.losses[self.one_shot_omniglot.dn]],
+                    feed_dict={self.dropout_rate: self.args.dropout_rate_value,
+                               self.support_set_images: support_set_images[0],
                                self.support_set_labels: support_set_labels[0], self.target_image: target_set_image[0],
                                self.target_label: target_set_label[0], self.training_phase: True,
                                self.learning_rate: self.current_learning_rate})
@@ -101,14 +110,14 @@ class ExperimentBuilder:
         total_val_accuracy = []
         with tqdm.tqdm(total=total_val_batches) as pbar:
             for sample_id, val_sample in enumerate(self.data.get_val_batches(total_batches=total_val_batches,
-                                                                                 augment_images=False)):
-
+                                                                             augment_images=False)):
                 support_set_images, target_set_image, support_set_labels, target_set_label = val_sample
 
                 c_loss_value, acc = sess.run(
                     [self.losses[self.one_shot_omniglot.classify],
                      self.losses[self.one_shot_omniglot.dn]],
-                    feed_dict={self.dropout_rate: 0.0, self.support_set_images: support_set_images[0],
+                    feed_dict={self.dropout_rate: self.args.dropout_rate_value,
+                               self.support_set_images: support_set_images[0],
                                self.support_set_labels: support_set_labels[0], self.target_image: target_set_image[0],
                                self.target_label: target_set_label[0], self.training_phase: False,
                                self.learning_rate: self.current_learning_rate})
@@ -138,13 +147,14 @@ class ExperimentBuilder:
         total_test_accuracy = []
         with tqdm.tqdm(total=total_test_batches) as pbar:
             for sample_id, test_sample in enumerate(self.data.get_test_batches(total_batches=total_test_batches,
-                                                                             augment_images=False)):
+                                                                               augment_images=False)):
                 support_set_images, target_set_image, support_set_labels, target_set_label = test_sample
 
                 c_loss_value, acc = sess.run(
                     [self.losses[self.one_shot_omniglot.classify],
                      self.losses[self.one_shot_omniglot.dn]],
-                    feed_dict={self.dropout_rate: 0.0, self.support_set_images: support_set_images[0],
+                    feed_dict={self.dropout_rate: self.args.dropout_rate_value,
+                               self.support_set_images: support_set_images[0],
                                self.support_set_labels: support_set_labels[0], self.target_image: target_set_image[0],
                                self.target_label: target_set_label[0], self.training_phase: False,
                                self.learning_rate: self.current_learning_rate})

one_shot_learning_network.py
@@ -3,8 +3,8 @@ import tensorflow.contrib.rnn as rnn
 from tensorflow.python.ops.nn_ops import max_pool, leaky_relu
 
 
-class BidirectionalLSTM:
-    def __init__(self, layer_sizes, batch_size):
+class g_embedding_bidirectionalLSTM:
+    def __init__(self, name, layer_sizes, batch_size):
         """
         Initializes a multi layer bidirectional LSTM
         :param layer_sizes: A list containing the neuron numbers per layer e.g. [100, 100, 100] returns a 3 layer, 100
@@ -14,8 +14,9 @@ class BidirectionalLSTM:
         self.reuse = False
         self.batch_size = batch_size
         self.layer_sizes = layer_sizes
+        self.name = name
 
-    def __call__(self, inputs, name, training=False):
+    def __call__(self, inputs, training=False):
         """
         Runs the bidirectional LSTM, produces outputs and saves both forward and backward states as well as gradients.
         :param inputs: The inputs should be a list of shape [sequence_length, batch_size, 64]
@@ -23,8 +24,9 @@ class BidirectionalLSTM:
         :param training: Flag that indicates if this is a training or evaluation stage
         :return: Returns the LSTM outputs, as well as the forward and backward hidden states.
         """
-        with tf.name_scope('bid-lstm' + name), tf.variable_scope('bid-lstm', reuse=self.reuse):
+        with tf.variable_scope(self.name, reuse=self.reuse):
             with tf.variable_scope("encoder"):
+
                 fw_lstm_cells_encoder = [rnn.LSTMCell(num_units=self.layer_sizes[i], activation=tf.nn.tanh)
                                          for i in range(len(self.layer_sizes))]
                 bw_lstm_cells_encoder = [rnn.LSTMCell(num_units=self.layer_sizes[i], activation=tf.nn.tanh)
@@ -36,22 +38,55 @@ class BidirectionalLSTM:
                     inputs,
                     dtype=tf.float32
                 )
-            print("out shape", tf.stack(outputs, axis=0).get_shape().as_list())
-            with tf.variable_scope("decoder"):
-                fw_lstm_cells_decoder = [rnn.LSTMCell(num_units=self.layer_sizes[i], activation=tf.nn.tanh)
-                                         for i in range(len(self.layer_sizes))]
-                bw_lstm_cells_decoder = [rnn.LSTMCell(num_units=self.layer_sizes[i], activation=tf.nn.tanh)
-                                         for i in range(len(self.layer_sizes))]
-                outputs, output_state_fw, output_state_bw = rnn.stack_bidirectional_rnn(
-                    fw_lstm_cells_decoder,
-                    bw_lstm_cells_decoder,
-                    outputs,
-                    dtype=tf.float32
-                )
+
+            print("g out shape", tf.stack(outputs, axis=1).get_shape().as_list())
 
         self.reuse = True
-        self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='bid-lstm')
-        return outputs, output_state_fw, output_state_bw
+        self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.name)
+        return outputs
+
+class f_embedding_bidirectionalLSTM:
+    def __init__(self, name, layer_size, batch_size):
+        """
+        Initializes a multi layer bidirectional LSTM
+        :param layer_sizes: A list containing the neuron numbers per layer e.g. [100, 100, 100] returns a 3 layer, 100
+                                                                                                        neuron bid-LSTM
+        :param batch_size: The experiments batch size
+        """
+        self.reuse = False
+        self.batch_size = batch_size
+        self.layer_size = layer_size
+        self.name = name
+
+    def __call__(self, support_set_embeddings, target_set_embeddings, K, training=False):
+        """
+        Runs the bidirectional LSTM, produces outputs and saves both forward and backward states as well as gradients.
+        :param inputs: The inputs should be a list of shape [sequence_length, batch_size, 64]
+        :param name: Name to give to the tensorflow op
+        :param training: Flag that indicates if this is a training or evaluation stage
+        :return: Returns the LSTM outputs, as well as the forward and backward hidden states.
+        """
+        b, k, h_g_dim = support_set_embeddings.get_shape().as_list()
+        b, h_f_dim = target_set_embeddings.get_shape().as_list()
+        with tf.variable_scope(self.name, reuse=self.reuse):
+            fw_lstm_cells_encoder = rnn.LSTMCell(num_units=self.layer_size, activation=tf.nn.tanh)
+            attentional_softmax = tf.ones(shape=(b, k)) * (1.0/k)
+            h = tf.zeros(shape=(b, h_g_dim)) + target_set_embeddings
+            h = (h, h)
+            for i in range(K):
+                attentional_softmax = tf.expand_dims(attentional_softmax, axis=2)
+                attented_features = support_set_embeddings * attentional_softmax
+                attented_features_summed = tf.reduce_sum(attented_features, axis=1)
+                x, h = fw_lstm_cells_encoder(inputs=attented_features_summed, state=h)
+                attentional_softmax = tf.layers.dense(x, units=k, activation=tf.nn.softmax, reuse=self.reuse)
+                self.reuse = True
+
+        outputs = x
+        print("out shape", tf.stack(outputs, axis=0).get_shape().as_list())
+        self.reuse = True
+        self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.name)
+        print(self.variables)
+        return outputs
 
 
 class DistanceNetwork:
@@ -101,8 +136,7 @@ class AttentionalClassify:
         """
         with tf.name_scope('attentional-classification' + name), tf.variable_scope('attentional-classification',
                                                                                    reuse=self.reuse):
-            softmax_similarities = tf.nn.softmax(similarities)
-            preds = tf.squeeze(tf.matmul(tf.expand_dims(softmax_similarities, 1), support_set_y))
+            preds = tf.squeeze(tf.matmul(tf.expand_dims(similarities, 1), support_set_y))
         self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='attentional-classification')
         return preds
 
@@ -130,7 +164,6 @@ class Classifier:
         :param dropout_rate: A tf placeholder of type tf.float32 indicating the amount of dropout applied
         :return: Embeddings of size [batch_size, 64]
         """
-
         with tf.variable_scope(self.name, reuse=self.reuse):
             outputs = image_input
             with tf.variable_scope('conv_layers'):
@@ -161,8 +194,8 @@ class Classifier:
 class MatchingNetwork:
     def __init__(self, support_set_images, support_set_labels, target_image, target_label, dropout_rate,
                  batch_size=100, num_channels=1, is_training=False, learning_rate=0.001, fce=False,
-                 num_classes_per_set=5,
-                 num_samples_per_class=1):
+                 full_context_unroll_k=5, num_classes_per_set=5, num_samples_per_class=1,
+                 average_per_class_embeddings=False):
 
         """
         Builds a matching network, the training and evaluation ops as well as data augmentation routines.
@@ -184,11 +217,15 @@ class MatchingNetwork:
         self.classifier = Classifier(name="classifier_net", batch_size=self.batch_size,
                             num_channels=num_channels, layer_sizes=[64, 64, 64, 64])
         if fce:
-            self.lstm = BidirectionalLSTM(layer_sizes=[32], batch_size=self.batch_size)
+            self.g_lstm = g_embedding_bidirectionalLSTM(name="g_lstm", layer_sizes=[32], batch_size=self.batch_size)
+            self.f_lstm = f_embedding_bidirectionalLSTM(name="f_attlstm", layer_size=64, batch_size=self.batch_size)
+
         self.dn = DistanceNetwork()
         self.classify = AttentionalClassify()
+        self.full_context_K = full_context_unroll_k
         self.support_set_images = support_set_images
         self.support_set_labels = support_set_labels
+        self.average_per_class_embeddings = average_per_class_embeddings
         self.target_image = target_image
         self.target_label = target_label
         self.dropout_rate = dropout_rate
@@ -206,24 +243,38 @@ class MatchingNetwork:
             [b, num_classes, spc] = self.support_set_labels[0].get_shape().as_list()
             self.support_set_labels = tf.reshape(self.support_set_labels[0], shape=(b, num_classes * spc))
             self.support_set_labels = tf.one_hot(self.support_set_labels, self.num_classes_per_set)  # one hot encode
-            encoded_images = []
+
+            g_encoded_images = []
+
             [b, num_classes, spc, h, w, c] = self.support_set_images[0].get_shape().as_list()
             self.support_set_images = tf.reshape(self.support_set_images[0], shape=(b, num_classes * spc, h, w, c))
+
             for image in tf.unstack(self.support_set_images, axis=1):  # produce embeddings for support set images
-                gen_encode = self.classifier(image_input=image, training=self.is_training, dropout_rate=self.dropout_rate)
-                encoded_images.append(gen_encode)
+                support_set_cnn_embed = self.classifier(image_input=image, training=self.is_training,
+                                                        dropout_rate=self.dropout_rate)
+                g_encoded_images.append(support_set_cnn_embed)
+
+            if self.average_per_class_embeddings:
+                g_encoded_images = tf.stack(g_encoded_images, axis=1)
+                b, k, h = g_encoded_images.get_shape().as_list()
+                g_encoded_images = tf.reshape(shape=(b, num_classes, spc, h))
+                g_encoded_images = tf.reduce_mean(g_encoded_images, axis=2)
+                self.support_set_labels = tf.reshape(self.support_set_labels, shape=(b, num_classes, spc,
+                                                                                     self.num_classes_per_set))
+                self.support_set_labels = tf.reduce_mean(self.support_set_labels, axis=2)
 
             target_image = self.target_image[0]  # produce embedding for target images
-            gen_encode = self.classifier(image_input=target_image, training=self.is_training, dropout_rate=self.dropout_rate)
 
-            encoded_images.append(gen_encode)
+            f_encoded_image = self.classifier(image_input=target_image, training=self.is_training,
+                                                   dropout_rate=self.dropout_rate)
 
             if self.fce:  # Apply LSTM on embeddings if fce is enabled
-                encoded_images, output_state_fw, output_state_bw = self.lstm(encoded_images, name="lstm",
-                                                                             training=self.is_training)
-            outputs = tf.stack(encoded_images)
-
-            similarities = self.dn(support_set=outputs[:-1], input_image=outputs[-1], name="distance_calculation",
+                g_encoded_images = self.g_lstm(g_encoded_images, training=self.is_training)
+                f_encoded_image = self.f_lstm(support_set_embeddings=tf.stack(g_encoded_images, axis=1),
+                                              K=self.full_context_K,
+                                              target_set_embeddings=f_encoded_image, training=self.is_training)
+            g_encoded_images = tf.stack(g_encoded_images, axis=0)
+            similarities = self.dn(support_set=g_encoded_images, input_image=f_encoded_image, name="distance_calculation",
                                    training=self.is_training)  # get similarity between support set embeddings and target
 
             preds = self.classify(similarities,
@@ -257,11 +308,11 @@ class MatchingNetwork:
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)  # Needed for correct batch norm usage
         with tf.control_dependencies(update_ops):  # Needed for correct batch norm usage
             if self.fce:
-                train_variables = self.lstm.variables + self.classifier.variables
+                train_variables = self.f_lstm.variables + self.g_lstm.variables + self.classifier.variables
             else:
                 train_variables = self.classifier.variables
             c_error_opt_op = c_opt.minimize(losses[self.classify],
-                                            var_list=train_variables)
+                                            var_list=train_variables, colocate_gradients_with_ops=True)
 
         return c_error_opt_op
 

train_one_shot_learning_matching_network.py
@@ -3,39 +3,34 @@ from experiment_builder import ExperimentBuilder
 import tensorflow.contrib.slim as slim
 import data as dataset
 import tqdm
-from storage import save_statistics, build_experiment_folder
+
+from utils.parser_utils import get_args
+from utils.storage import save_statistics, build_experiment_folder
 
 tf.reset_default_graph()
-
-# Experiment Setup
-batch_size = 32
-fce = False
-classes_per_set = 20
-samples_per_class = 1
-continue_from_epoch = -1  # use -1 to start from scratch
-epochs = 200
-num_gpus = 1
-logs_path = "one_shot_outputs/"
-experiment_name = "one_shot_learning_embedding_{}_{}".format(samples_per_class, classes_per_set)
-
+args = get_args()
 # Experiment builder
-data = dataset.FolderDatasetLoader(num_of_gpus=num_gpus, batch_size=batch_size, image_height=28, image_width=28,
+data = dataset.FolderDatasetLoader(num_of_gpus=1, batch_size=args.batch_size, image_height=28, image_width=28,
                                    image_channels=1,
                                    train_val_test_split=(1200/1622, 211/1622, 211/162),
                                    samples_per_iter=1, num_workers=4,
                                    data_path="datasets/omniglot_data", name="omniglot_data",
                                    index_of_folder_indicating_class=-2, reset_stored_filepaths=False,
-                                   num_samples_per_class=samples_per_class, num_classes_per_set=classes_per_set)
+                                   num_samples_per_class=args.samples_per_class, num_classes_per_set=args.classes_per_set)
 
 experiment = ExperimentBuilder(data)
-one_shot_omniglot, losses, c_error_opt_op, init = experiment.build_experiment(batch_size,
-                                                                                     classes_per_set,
-                                                                                     samples_per_class, fce)
-total_train_batches = 1000
-total_val_batches = 1000
-total_test_batches = 1000
+one_shot_omniglot, losses, c_error_opt_op, init = experiment.build_experiment(args.batch_size,
+                                                                              args.classes_per_set,
+                                                                              args.samples_per_class,
+                                                                              args.use_full_context_embeddings,
+                                                                              full_context_unroll_k=
+                                                                              args.full_context_unroll_k,
+                                                                              args=args)
+total_train_batches = args.total_iter_per_epoch
+total_val_batches = args.total_iter_per_epoch
+total_test_batches = args.total_iter_per_epoch
 
-saved_models_filepath, logs_filepath = build_experiment_folder(experiment_name)
+saved_models_filepath, logs_filepath = build_experiment_folder(args.experiment_title)
 
 
 save_statistics(logs_filepath, ["epoch", "total_train_c_loss_mean", "total_train_c_loss_std",
@@ -49,8 +44,8 @@ with tf.Session() as sess:
     sess.run(init)
     train_saver = tf.train.Saver()
     val_saver = tf.train.Saver()
-    if continue_from_epoch != -1: #load checkpoint if needed
-        checkpoint = "saved_models/{}_{}.ckpt".format(experiment_name, continue_from_epoch)
+    if args.continue_from_epoch != -1: #load checkpoint if needed
+        checkpoint = "saved_models/{}_{}.ckpt".format(args.experiment_title, args.continue_from_epoch)
         variables_to_restore = []
         for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
             print(var)
@@ -63,11 +58,11 @@ with tf.Session() as sess:
             variables_to_restore,
             ignore_missing_vars=True)
         fine_tune(sess)
-
+    start_epoch = args.continue_from_epoch if args.continue_from_epoch != -1 else 0
     best_val_acc_mean = 0.
     best_val_epoch = 6
-    with tqdm.tqdm(total=epochs) as pbar_e:
-        for e in range(0, epochs):
+    with tqdm.tqdm(total=args.total_epochs) as pbar_e:
+        for e in range(start_epoch, args.total_epochs):
             total_train_c_loss_mean, total_train_c_loss_std, total_train_accuracy_mean, total_train_accuracy_std =\
                 experiment.run_training_epoch(total_train_batches=total_train_batches,
                                                                                 sess=sess)
@@ -85,7 +80,7 @@ with tf.Session() as sess:
                 best_val_acc_mean = total_val_accuracy_mean
                 best_val_epoch = e
 
-                val_save_path = val_saver.save(sess, "{}/best_val_{}_{}.ckpt".format(saved_models_filepath, experiment_name, e))
+                val_save_path = val_saver.save(sess, "{}/best_val_{}_{}.ckpt".format(saved_models_filepath, args.experiment_title, e))
 
                 total_test_c_loss_mean, total_test_c_loss_std, total_test_accuracy_mean, total_test_accuracy_std \
                     = -1, -1, -1, -1
@@ -97,11 +92,11 @@ with tf.Session() as sess:
                              total_test_c_loss_mean, total_test_c_loss_std, total_test_accuracy_mean,
                              total_test_accuracy_std])
 
-            save_path = train_saver.save(sess, "{}/{}_{}.ckpt".format(saved_models_filepath, experiment_name, e))
+            save_path = train_saver.save(sess, "{}/{}_{}.ckpt".format(saved_models_filepath, args.experiment_title, e))
             pbar_e.update(1)
 
         val_saver.restore(sess,
-                          "{}/best_val_{}_{}.ckpt".format(saved_models_filepath, experiment_name, best_val_epoch))
+                          "{}/best_val_{}_{}.ckpt".format(saved_models_filepath, args.experiment_title, best_val_epoch))
         total_test_c_loss_mean, total_test_c_loss_std, total_test_accuracy_mean, total_test_accuracy_std = \
             experiment.run_testing_epoch(total_test_batches=total_test_batches,
                                          sess=sess)